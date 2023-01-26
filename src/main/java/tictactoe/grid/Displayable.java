9f93e4dddf58625876ffbab7cb653f951d09c5c7
Energisse <thomas.halvick@gmail.com>
2023-01-26 16:02:29 +0100

    modification des grilles

--- src/main/java/tictactoe/grid/Displayable.java
+++ src/main/java/tictactoe/grid/Displayable.java
@@ -1,9 +1,9 @@
-package tictactoe;
+package tictactoe.grid;
 
 /**
  * interface Displayable of every components that need to be displayed
  * @author Halvick Thomas
- * @version 1
+ * @version 1.1
  */
 public interface Displayable {
 
